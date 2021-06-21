c903c095b9f442a40467873da76a1b78484d9946
HimeshNayak <himeshnayak015@gmail.com>
2021-06-21 23:39:41 +0530

    created editProfile

---
+++ lib/screens/editProfile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+
+class EditProfile extends StatefulWidget {
+  final UserData user;
+  EditProfile({required this.user});
+
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
