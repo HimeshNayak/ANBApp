48f14a729d852e4d8b928a6c155d3c7888c55613
HimeshNayak <himeshnayak015@gmail.com>
2021-06-24 14:59:25 +0530

    added logic to rename and store in Firestore and SP

--- lib/screens/editProfile.dart
+++ lib/screens/editProfile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/user.dart';
+import '../widgets/commonWidgets.dart';
 
 class EditProfile extends StatefulWidget {
   final UserData user;
@@ -11,8 +14,58 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  TextEditingController nameController = new TextEditingController();
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                  ),
+                ),
+                OutlinedButton(
+                  child: Text('Save'),
+                  onPressed: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    if (nameController.value.text.isNotEmpty) {
+                      await SharedPreferences.getInstance().then((_prefs) {
+                        _prefs.setString('userName', nameController.value.text);
+                      });
+                      await FirebaseFirestore.instance
+                          .collection('admins')
+                          .doc(widget.user.uid)
+                          .update({
+                        'username': nameController.value.text.toString(),
+                      }).whenComplete(() {
+                        setState(() {
+                          isLoading = false;
+                        });
+                        Navigator.pop(context);
+                      });
+                    } else {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      print('Name is empty!!! Please enter your name');
+                    }
+                  },
+                ),
+              ],
+            ),
+            overlayProgress(context: context, visible: isLoading)
+          ],
+        ),
+      ),
+    );
   }
 }
