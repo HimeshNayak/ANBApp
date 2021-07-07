32e31d156001df325d37f1edd1c3bdb72955a1e5
HimeshNayak <himeshnayak015@gmail.com>
2021-07-07 23:09:40 +0530

    created uploadPhoto class

--- lib/screens/patientProfile.dart
+++ lib/screens/patientProfile.dart
@@ -1,15 +1,10 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:firebase_storage/firebase_storage.dart' as fs;
 
 import '../models/user.dart';
 import '../services/auth.dart';
+import '../services/uploadPhoto.dart';
 import '../style/fonts.dart';
 import '../widgets/commonWidgets.dart';
 import '../widgets/widgets.dart';
@@ -63,72 +58,6 @@ class _PatientProfileState extends State<PatientProfile> {
     );
   }
 
-  PickedFile? _pickedFile;
-  PickedFile? get pickedFile => _pickedFile;
-  String? _imageName = '';
-  String? get imageName => _imageName;
-  File? _filePath;
-  File? get filePath => _filePath;
-  String? uploadedImageUrl = '';
-
-  Future<void> openCamera() async {
-    final imagePicker = ImagePicker();
-
-    await Permission.photos.request();
-
-    var permissionStatus = await Permission.photos.status;
-    if (permissionStatus.isGranted) {
-      _pickedFile = await imagePicker.getImage(source: ImageSource.gallery);
-      if (_pickedFile != null) {
-        _filePath = File(_pickedFile?.path as String);
-        _imageName = _filePath?.uri.path.split('/').last;
-        print(_filePath);
-        print(_imageName);
-      } else {
-        print("Please pick Image");
-      }
-    } else {
-      print('Permission Granted');
-    }
-  }
-
-  Future<fs.UploadTask?> sendImageToFirebase() async {
-    if (_pickedFile == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('No file was selected'),
-        ),
-      );
-      return null;
-    }
-
-    fs.UploadTask uploadTask;
-
-    // Create a Reference to the file
-    fs.Reference ref = fs.FirebaseStorage.instance
-        .ref()
-        .child(widget.user.email.toString())
-        .child('/$_imageName');
-
-    final metadata = fs.SettableMetadata(
-        contentType: 'image/jpeg',
-        customMetadata: {'picked-file-path': _pickedFile?.path as String});
-
-    uploadTask = ref.putFile(File(_pickedFile?.path as String), metadata);
-
-    return Future.value(uploadTask);
-  }
-
-  Future<void> updateImageUrl(String url) async {
-    await FirebaseFirestore.instance
-        .collection((widget.isAdmin) ? 'admins' : 'users')
-        .doc(widget.user.uid)
-        .update({'photoUrl': url});
-    await SharedPreferences.getInstance().then((prefs) {
-      prefs.setString('photoUrl', url);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -165,11 +94,20 @@ class _PatientProfileState extends State<PatientProfile> {
                                 setState(() {
                                   isLoading = true;
                                 });
-                                openCamera().whenComplete(() {
-                                  sendImageToFirebase().then((value) {
+                                UploadPhoto up = new UploadPhoto();
+                                up.openCamera().whenComplete(() {
+                                  up
+                                      .sendImageToFirebase(
+                                          context, widget.user.email.toString())
+                                      .then((value) {
                                     value?.then((v) {
                                       v.ref.getDownloadURL().then((value) {
-                                        updateImageUrl(value).whenComplete(() {
+                                        up
+                                            .updateImageUrl(
+                                                widget.isAdmin,
+                                                widget.user.uid.toString(),
+                                                value)
+                                            .whenComplete(() {
                                           setState(() {
                                             widget.user.photoUrl = value;
                                             isLoading = false;
---
+++ lib/services/uploadPhoto.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as fs;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UploadPhoto {
+  PickedFile? _pickedFile;
+  PickedFile? get pickedFile => _pickedFile;
+  String? _imageName = '';
+  String? get imageName => _imageName;
+  File? _filePath;
+  File? get filePath => _filePath;
+  String? uploadedImageUrl = '';
+
+  Future<void> openCamera() async {
+    final imagePicker = ImagePicker();
+
+    await Permission.photos.request();
+
+    var permissionStatus = await Permission.photos.status;
+    if (permissionStatus.isGranted) {
+      _pickedFile = await imagePicker.getImage(source: ImageSource.gallery);
+      if (_pickedFile != null) {
+        _filePath = File(_pickedFile?.path as String);
+        _imageName = _filePath?.uri.path.split('/').last;
+        print(_filePath);
+        print(_imageName);
+      } else {
+        print("Please pick Image");
+      }
+    } else {
+      print('Permission Granted');
+    }
+  }
+
+  Future<fs.UploadTask?> sendImageToFirebase(
+      BuildContext context, String email) async {
+    if (_pickedFile == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No file was selected'),
+        ),
+      );
+      return null;
+    }
+
+    fs.UploadTask uploadTask;
+
+    // Create a Reference to the file
+    fs.Reference ref =
+        fs.FirebaseStorage.instance.ref().child(email).child('/$_imageName');
+
+    final metadata = fs.SettableMetadata(
+        contentType: 'image/jpeg',
+        customMetadata: {'picked-file-path': _pickedFile?.path as String});
+
+    uploadTask = ref.putFile(File(_pickedFile?.path as String), metadata);
+
+    return Future.value(uploadTask);
+  }
+
+  Future<void> updateImageUrl(bool isAdmin, String uid, String url) async {
+    await FirebaseFirestore.instance
+        .collection((isAdmin) ? 'admins' : 'users')
+        .doc(uid)
+        .update({'photoUrl': url});
+    await SharedPreferences.getInstance().then((prefs) {
+      prefs.setString('photoUrl', url);
+    });
+  }
+}
